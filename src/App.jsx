f7f3c7e0378a0b4791a7604639d6f753cfc8410c
MarinhSt <st_marinh@protonmail.com>
2024-07-04 20:28:16 -0500

    first step ready, form to obtain the films

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [searchBox, setSearch] = useState('')
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handelSearch = e => {
+        e.preventDefault()
+        console.log(searchBox)
+    }
+    return (
+        <>
+            <header>
+                <h1>Movie Search</h1>
+                <form onSubmit={handelSearch} className="searchbox">
+                    <input
+                        type="text"
+                        onChange={({ target }) => setSearch(target.value)}
+                        value={searchBox}
+                        placeholder="fast and furious, star wars ..."
+                    />
+                    <button type="submit">
+                        <i className="  material-icons">search</i>
+                    </button>
+                </form>
+            </header>
+            <main>
+                <p>here we will display our movie list</p>
+            </main>
+        </>
+    )
 }
 
 export default App
